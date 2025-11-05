719dc024032e99c1702277eef062f31231aca2ba
Diogo Gaspar <diogommgaspar@gmail.com>
2025-11-05 00:47:34 +0000

    FUNCIONA TUDO!!!

--- Mambos_Postgres/Apagar_trigger.sql
+++ Mambos_Postgres/Apagar_trigger.sql
@@ -1,27 +1,31 @@
--- DROP ALL TRIGGERS
-DROP TRIGGER IF EXISTS trg_calc_num_dias_ferias ON ferias;
-DROP TRIGGER IF EXISTS trg_cria_utilizador ON funcionarios;
-DROP TRIGGER IF EXISTS trg_delete_permissoes ON funcionarios;
-DROP TRIGGER IF EXISTS trg_incrementa_funcionarios ON funcionarios;
-DROP TRIGGER IF EXISTS trg_update_num_candidatos ON candidato_a;
-DROP TRIGGER IF EXISTS trg_decrease_num_candidatos ON candidato_a;
+SET search_path TO bd054_schema, public;
 
--- DROP ALL TRIGGER FUNCTIONS
-DROP FUNCTION IF EXISTS calc_num_dias_ferias() CASCADE;
-DROP FUNCTION IF EXISTS cria_utilizador() CASCADE;
-DROP FUNCTION IF EXISTS delete_permissoes() CASCADE;
-DROP FUNCTION IF EXISTS incrementa_funcionarios() CASCADE;
-DROP FUNCTION IF EXISTS update_num_candidatos() CASCADE;
-DROP FUNCTION IF EXISTS decrease_num_candidatos() CASCADE;
+-- 1) Triggers
+DROP TRIGGER IF EXISTS trg_calc_salario_liquido ON bd054_schema.salario;
+DROP TRIGGER IF EXISTS trg_validar_dias_ferias ON bd054_schema.ferias;
+DROP TRIGGER IF EXISTS trg_registrar_mudanca_cargo ON bd054_schema.funcionarios;
+DROP TRIGGER IF EXISTS trg_validar_idade_funcionario ON bd054_schema.funcionarios;
+DROP TRIGGER IF EXISTS trg_cria_utilizador ON bd054_schema.funcionarios;
+DROP TRIGGER IF EXISTS trg_delete_permissoes ON bd054_schema.funcionarios;
 
--- DROP ALL OTHER FUNCTIONS
-DROP FUNCTION IF EXISTS calc_idade(DATE) CASCADE;
-DROP FUNCTION IF EXISTS calcular_num_dias_ferias(INT, DATE, DATE) CASCADE;
-DROP FUNCTION IF EXISTS calcular_total_dias_permitidos(INT) CASCADE;
+-- 2) Procedures
+DROP PROCEDURE IF EXISTS bd054_schema.aprovar_ferias_proc(INT, DATE);
+DROP PROCEDURE IF EXISTS bd054_schema.adicionar_candidatura_proc(INT, INT, INT);
+DROP PROCEDURE IF EXISTS bd054_schema.adicionar_uma_formacao(INT, VARCHAR, VARCHAR, DATE, DATE, VARCHAR);
+DROP PROCEDURE IF EXISTS bd054_schema.adicionar_avaliacao(INT, INT, DATE, BYTEA, VARCHAR, VARCHAR);
+DROP PROCEDURE IF EXISTS bd054_schema.adicionar_funcionario_proc(VARCHAR, VARCHAR, VARCHAR, VARCHAR, VARCHAR, VARCHAR, VARCHAR, VARCHAR, DATE, VARCHAR, INT);
 
--- DROP ALL PROCEDURES
-DROP PROCEDURE IF EXISTS aprovar_ferias_proc(INT, DATE) CASCADE;
-DROP PROCEDURE IF EXISTS adicionar_candidatura_proc(INT, INT, INT) CASCADE;
-DROP PROCEDURE IF EXISTS adicionar_uma_formacao(INT, VARCHAR, VARCHAR, DATE, DATE, VARCHAR) CASCADE;
-DROP PROCEDURE IF EXISTS adicionar_avaliacao(INT, INT, DATE, BYTEA, VARCHAR, VARCHAR) CASCADE;
-DROP PROCEDURE IF EXISTS adicionar_funcionario_proc(VARCHAR, VARCHAR, VARCHAR, VARCHAR, VARCHAR, VARCHAR, VARCHAR, VARCHAR, DATE, VARCHAR, INT) CASCADE;
+-- 3) Functions (inclui funções de trigger)
+DROP FUNCTION IF EXISTS bd054_schema.calc_salario_liquido();
+DROP FUNCTION IF EXISTS bd054_schema.validar_dias_ferias();
+DROP FUNCTION IF EXISTS bd054_schema.registrar_mudanca_cargo();
+DROP FUNCTION IF EXISTS bd054_schema.validar_idade_funcionario();
+DROP FUNCTION IF EXISTS bd054_schema.cria_utilizador();
+DROP FUNCTION IF EXISTS bd054_schema.delete_permissoes();
+
+DROP FUNCTION IF EXISTS bd054_schema.calcular_num_total_funcionarios(INT);
+DROP FUNCTION IF EXISTS bd054_schema.calcular_num_funcionarios_departamento(INT);
+DROP FUNCTION IF EXISTS bd054_schema.calcular_num_aderentes_formacao(INT);
+DROP FUNCTION IF EXISTS bd054_schema.calc_idade(DATE);
+DROP FUNCTION IF EXISTS bd054_schema.calcular_num_dias_ferias(INT, DATE, DATE);
+DROP FUNCTION IF EXISTS bd054_schema.calcular_total_dias_permitidos(INT);
