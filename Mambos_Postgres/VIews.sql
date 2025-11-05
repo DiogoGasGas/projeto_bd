4db0e08dea32f03ec76d236f03cf9c9b3c057c01
Mariana <mariana.d.ferreira.2005@gmail.com>
2025-11-05 16:48:44 +0000

    sim

--- Mambos_Postgres/VIews.sql
+++ Mambos_Postgres/VIews.sql
@@ -1,6 +1,26 @@
-set search_path to bd054_schema, public;
+
 
 -- faz uma tabela virtual com os atributos (id do fun, nome completo, nome do departamento, cargo e total de funcionarios no departamento)
+
+
+select view * from vw_funcionarios_departamentos;
+-- Obtém se os funcionários que a sua remuneração total
+CREATE OR REPLACE VIEW vw_remun_completa AS
+SELECT 
+  f.id_fun,
+  f.primeiro_nome || ' ' || f.ultimo_nome AS nome_completo,
+  (salario_liquido)+ COALESCE(sum(b.valor),0) as remun_completa
+
+FROM funcionarios As f 
+LEFT JOIN salario AS s 
+ON f.id_fun = s.id_fun
+LEFT JOIN beneficios as b
+ON f.id_fun = b.id_fun
+GROUP BY f.id_fun, nome_completo, salario_liquido
+ORDER By f.id_fun ASC;
+
+
+
 
 
 
@@ -19,10 +39,16 @@ JOIN departamentos d ON f.id_depart = d.id_depart
 JOIN salario s ON f.id_fun = s.id_fun;
 
 
+
+
+
+
+
+
 SET search_path TO bd054_schema, public;
 create or replace view ferias_aprovadas as
 select f.id_fun, f.primeiro_nome || ' ' || f.ultimo_nome as nome_funcionario,
-       fe.data_inicio, fe.data_fim
+       fe.data_inicio, fe.data_fim, calcular_num_dias_ferias(fe.id_fun, fe.data_inicio, fe.data_fim) as num_dias_ferias
 from funcionarios f
 join ferias fe on f.id_fun = fe.id_fun
 where fe.estado_aprov = 'Aprovado';
@@ -40,33 +66,39 @@ JOIN funcionarios f ON f.id_depart = d.id_depart
 JOIN salario s ON s.id_fun = f.id_fun
 GROUP BY d.id_depart, d.nome;
 
+SET search_path TO bd054_schema, public;
+select * from vw_formacoes_funcionarios;
 
 
 
 
-set search_path to bd054_schema, public;
+
+SET search_path TO bd054_schema, public;
+
 CREATE OR REPLACE VIEW vw_formacoes_funcionarios AS
+
 SELECT 
     f.id_fun,
     f.primeiro_nome || ' ' || f.ultimo_nome AS nome_funcionario,
-    fo.titulo AS formacao,
-    fa.data_inicio,
-    fa.data_fim
+    fo.id_for,
+    fo.nome_formacao AS formacao,
+    fo.data_inicio,
+    fo.data_fim,
+    calcular_num_aderentes_formacao(fo.id_for) AS total_aderentes
 FROM funcionarios f
-JOIN formacao_aderentes fa ON fa.id_fun = f.id_fun
-JOIN formacoes fo ON fo.id_for = fa.id_for;
+JOIN teve_formacao fa ON f.id_fun = fa.id_fun
+JOIN formacoes fo ON fa.id_for = fo.id_for;
 
 
-
+set search_path to bd054_schema, public;
 CREATE OR REPLACE VIEW vw_vagas_candidatos AS
 SELECT 
     v.id_vaga,
-    v.titulo AS vaga,
     v.estado,
     COUNT(c.id_cand) AS num_candidatos
 FROM vagas v
 LEFT JOIN candidato_a c ON v.id_vaga = c.id_vaga
-GROUP BY v.id_vaga, v.titulo, v.estado;
+GROUP BY v.id_vaga, v.estado;
 
 
 set search_path to bd054_schema, public;
@@ -78,18 +110,3 @@ SELECT
     (SELECT COUNT(*) FROM formacoes) AS total_formacoes;
 
 
-select view * from vw_funcionarios_departamentos;
--- Obtém se os funcionários que a sua remuneração total
-CREATE OR REPLACE VIEW vw_remun_completa AS
-SELECT 
-  f.id_fun,
-  f.primeiro_nome || ' ' || f.ultimo_nome AS nome_completo,
-  (salario_liquido)+ COALESCE(sum(b.valor),0) as remun_completa
-
-FROM funcionarios As f 
-LEFT JOIN salario AS s 
-ON f.id_fun = s.id_fun
-LEFT JOIN beneficios as b
-ON f.id_fun = b.id_fun
-GROUP BY f.id_fun, nome_completo, salario_liquido
-ORDER By f.id_fun ASC;
