9a3ce4732531c19d03e1c7fc2ff9b49e99e040ae
Mariana <mariana.d.ferreira.2005@gmail.com>
2025-11-05 11:55:52 +0000

    aaa

--- Mambos_Postgres/VIews.sql
+++ Mambos_Postgres/VIews.sql
@@ -1,1 +1,11 @@
---ola
+
+CREATE OR REPLACE VIEW vw_funcionarios_departamentos AS
+SELECT 
+    f.id_fun,
+    f.primeiro_nome || ' ' || f.ultimo_nome AS nome_funcionario,
+    d.nome_depart AS departamento,
+    d.num_funcionarios AS total_no_departamento
+FROM funcionarios f
+JOIN departamentos d ON f.id_depart = d.id_depart;
+
+
